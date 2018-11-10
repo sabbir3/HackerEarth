7fddfc2bf104e8249110fa296a1ff038cd312b61
Sabbir Ahmed <sabbir12003@gmail.com>
2018-11-10 21:27:26 +0600

    soting my hackerearth array solution for the first time

---
+++ ZF01.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,j,n;
+    cin>>n;
+    int arr[n];
+
+    for(i=0; i<n; i++){
+
+        cin>>arr[i];
+    }
+
+    for(i=n-1; i>=0; i--){
+
+        cout<<arr[i]<<"\n";
+    }
+
+    return 0;
+}
